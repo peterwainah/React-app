fd7fe1f15d0e39deb10fca9fdf8e2ff2150b3f49
peterwainaina <petwah17@gmail.com>
2021-08-23 15:04:01 +0300

    add links for template and downloadable dashboard pdf

---
+++ datahandbook/src/Layout/Home.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+class Home extends Component {
+    render() {
+	    return (
+		<div className="row ">
+	           <div className="medium-12 columns">
+                   </div>
+                </div>  
+	     );
+    }
+}
+export default Home;
